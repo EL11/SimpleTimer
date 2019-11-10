416a61531cf3f2732d19c8f0eb85bdb44e5805e0
EL11 <45426745+EL11@users.noreply.github.com>
2019-11-10 11:45:55 +0100

    SimpleTimer v1.0 commented
    Commented.
    Removed unused variables.

--- buzz.ino
+++ buzz.ino
@@ -11,49 +11,49 @@
 #include <gfxfont.h>
 
 //--DISPLAY
-#define SSD1306_128_32 //Matrice display
-#define OLED_ADDR 0x3C //Indirizzo display
-Adafruit_SSD1306 display(-1); //Reset Pin non utilizzato
+#define SSD1306_128_32 //Display matrix
+#define OLED_ADDR 0x3C //Display address
+Adafruit_SSD1306 display(-1); //Reset pin not used
 
 
 //--I/O
-const int buzzer = 8;
-const int pot = A1;
+const int buzzer = 8; //+ of piezoelectric buzzer 3-24 VDC. Needs power transistor to erogate up to 24V with external power supply.
+const int pot = A1; //Central potentiometer pin to A1 - B1K type suggested, if using a different type please change the potMap threshold at line 195.
 
 //Buffer
-int potReadings[200]={};
+int potReadings[200]={}; //Array with untrustable measurements
 
 //Globals
-int potVal;
-int potMap;
-String UOM="SECONDI";
+int potVal; //Raw potentiometer value
+int potMap; //Mapped potentiometer value
+String UOM="SECONDS"; //Unit of measure
 
 //RAM
-int pot_past=1;
-int setTime=0;
-unsigned long time;
-unsigned long passedTime;
-unsigned long interval;
+int pot_past=1; //Initialize past potentiometer measurement
+int setTime=0; //Initialize user-set time
+unsigned long time; //Initialize memory for program execution time
+
 
 //Ks
-int factor;
-int coeff;
-int cd_factor;
-int buzz_flag=0;
+int factor; //Speed at which setTime decreases or increases
+int cd_factor; //Resolution of time decrement (1s default)
+int buzz_flag=0; //Bool for buzzer activation
 
 //Counters
-int count;
-int static_counter;
-int parallel_counter;
+int count; //Generic counter
+int static_counter; //Counter of seconds when potentiometer is static
+int parallel_counter; //Parallel counter to use at will
 
 
 
 
 void setup() {
    Serial.begin(9600);
+   //Initialize I/O
    pinMode(buzzer,OUTPUT);
    pinMode(pot,INPUT);
-   display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
+   //Initialize display and first screen
+   display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR); //Initialize display object
    display.clearDisplay();
    display.setFont(&FreeSerif12pt7b);
    display.setTextSize(1);
@@ -65,10 +65,11 @@ void setup() {
    display.setTextSize(1);
    display.setTextColor(WHITE);
    display.setCursor(90, 14);
-   display.print("v1.0"); //VERSIONE
+   display.print("v1.0"); //VERSION
    display.display();
+   //Arbitrary amount of time before start
    delay(5000);
-   display.setFont();
+   display.setFont(); //Set font back to default for next use
 }
 
 //Display function
@@ -83,7 +84,7 @@ void displ(String TXT, String KEY, int COLOR, String KEY2, int COLOR2) {
   display.setCursor(27, 3);
   display.print(KEY);
   display.display();
-  //display.setFont(&FreeSans9pt7b);
+  //display.setFont(&FreeSans9pt7b); uncomment to change the font for KEY2
   display.setTextSize(1);
   display.setTextColor(COLOR2);
   display.setCursor(97, 5);
@@ -92,61 +93,63 @@ void displ(String TXT, String KEY, int COLOR, String KEY2, int COLOR2) {
   display.setFont();
 }
 
-//RecordoPot methods
+//RecordPot methods
 void recordPot_staticCheck(){
      if(setTime>0){
         setTime=setTime;
         factor=0;
-        if(static_counter==9){
+        if(static_counter==9){ //After 9 seconds starts the countdown
            cd_factor=-1;
       }
       }
      
 }
 
-//Confronta misurazione passata e presente, inizia conteggio se valore statico
+//Record measurements and handle static check
 void recordPot(int pot_curr){
      Serial.println("Static counter:" + String(static_counter));
      Serial.println("Factor:" + String(factor));
      Serial.println("Countdown:" + String(cd_factor));
-     //Checks
-     
+   
+     //--CHECKS
+     //Check is pot_past has being recorded for the first time
      if(pot_past==1){
         pot_past=pot_curr;
      }
+     //Check if value is static, user is not moving the potentiometer for an amount of time (2s default)
      if(static_counter>=2){
       recordPot_staticCheck();
      }
-     
+     //Check if countdown has stopped. TODO: distinct between starting zero condition and zero after countdown condition
      if(setTime<=0){
       cd_factor=0;
-      buzz_flag=1;
+      buzz_flag=1; //Enable buzzer permission to operate
      }
 
-     //Increments   
+     //--INCREMENTS
+     //Check if potentiometer value is increasing
      if(pot_curr>pot_past){
         if(pot_curr-pot_past<=5){ 
            pot_curr=pot_past;
         }
         buzz_flag=0;
         static_counter=0;
-        factor=1;
+        factor=1; 
         setTime=setTime+factor;   
         pot_past=pot_curr;
-        //Accelerate over threshold
+        //Accelerate x5 over threshold (15s default)
         if(setTime>15){
           factor=5;
         }
         
-        
+      
      }
+     //Check if potentiometer value is decreasing
      if(pot_curr<pot_past){
         if(pot_past-pot_curr<=5){
            pot_curr=pot_past;
         }
-       //static_counter=0;
-       factor=0;
-       //cd_factor=0;
+       factor=0; //Do not decrease setTime, instead stop increment for more accurate control. TODO: implement setTime decreasing
         if(setTime>0){
            setTime=setTime+factor;
         } 
@@ -163,7 +166,7 @@ void recordPot(int pot_curr){
          static_counter=static_counter+1;
       }
       if(static_counter==10){
-         static_counter=0;
+         static_counter=0; //Reset static counter on the 10th second
          
       }
      }
@@ -171,6 +174,8 @@ void recordPot(int pot_curr){
      
 }
 
+//--FILTERs
+//Remove first measurements
 void garbageFilter(){
   time = millis();
   for (count=0;count<=199;count++){
@@ -178,9 +183,10 @@ void garbageFilter(){
     }
 }
 
+//--DISPLAY VIEW
 void handleDisp(){
   displ(String(setTime),UOM,1,String(time/1000),1);
-  delay(700);
+  delay(700); //Main interrupt. If you change this value, check the countdown timing is still correct.
 }
 
 void loop() {
@@ -189,15 +195,15 @@ void loop() {
     UOM="PAUSA";
     if(potVal!=0 && potVal!=1){
        UOM="SECONDI";
-       potMap=map(potVal,2,900,1,100);
-       recordPot(potMap);
+       potMap=map(potVal,2,900,1,100); //Map raw potentiometer values to smoother virtual values
+       recordPot(potMap); 
        Serial.println(setTime);
        Serial.println(potMap);
        Serial.println("Buzzer:" + String(buzz_flag));
        if(buzz_flag==1){
-          tone(buzzer,1000);
-          delay(200);
-          tone(buzzer,500);
+          tone(buzzer,1000); //Buzzer high pitch
+          delay(200); //Timing for buzzer sound. Connected to main interrupt, if you change this value timing may be incorrect.
+          tone(buzzer,500); //Buzzer low pitch
      }
        if(buzz_flag==0){
           pinMode(buzzer,LOW);
